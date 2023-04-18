edf9b31ba0544fd435dee7c3c1590543cc9b5d14
sukmati subba <sukmatilimboo7@gmail.com>
2023-04-18 04:25:12 -0700

    #area of rectangle.py

---
+++ #area of rectangle.py
@@ -0,0 +1,6 @@
+#area of rectangle
+width =float(input("Enter the no: "))
+height =float(input("enter the number: "))
+area = width * height
+print("the result is ",area)
+               
---
+++ tempCodeRunnerFile.python
@@ -0,0 +1,5 @@
+pi = 3.14
+r = float (input('enter a number for witdh '))
+h = float (input('enter a number for height '))
+volume = pi * (r**2)*h
+print('the result is ',volume)
